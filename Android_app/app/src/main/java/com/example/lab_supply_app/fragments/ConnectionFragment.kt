d574cad40c55124bc03cf7aa1678f71de032b08f
Gartosz <bartekg001@gmail.com>
2023-04-11 15:08:43 +0200

    Add binding and basic overrides to fragments.

--- Android_app/app/src/main/java/com/example/lab_supply_app/fragments/ConnectionFragment.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/fragments/ConnectionFragment.kt
@@ -1,6 +1,27 @@
 package com.example.lab_supply_app.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.lab_supply_app.databinding.ConnectionLayoutBinding
+import com.example.lab_supply_app.models.ConnectionsViewModel
 
 class ConnectionFragment : Fragment() {
+    private lateinit var binding: ConnectionLayoutBinding
+    private val cocktailViewModel: ConnectionsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ConnectionLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- Android_app/app/src/main/java/com/example/lab_supply_app/fragments/RWFragment.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/fragments/RWFragment.kt
@@ -1,6 +1,27 @@
 package com.example.lab_supply_app.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.lab_supply_app.databinding.RwValuesLayoutBinding
+import com.example.lab_supply_app.models.RWViewModel
 
 class RWFragment : Fragment() {
+    private lateinit var binding: RwValuesLayoutBinding
+    private val cocktailViewModel: RWViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = RwValuesLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
