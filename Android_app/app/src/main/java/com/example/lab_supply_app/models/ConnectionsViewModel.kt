2b13ad686ba90b9ac8a17343f675619ff1d79539
Bartosz <bartekg001@gmail.com>
2023-04-13 01:05:24 +0200

    Store bluetooth state as livedata in view model.

--- Android_app/app/src/main/java/com/example/lab_supply_app/models/ConnectionsViewModel.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/models/ConnectionsViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.lab_supply_app.models
 
 import android.app.Application
+import android.bluetooth.BluetoothAdapter
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 
 class ConnectionsViewModel(application: Application) : AndroidViewModel(application) {
+    lateinit var bluetoothState : LiveData<Boolean>
+
+    fun setBTState(bluetoothAdapter: BluetoothAdapter)
+    {
+        bluetoothState = liveData { bluetoothAdapter.isEnabled }
+    }
 }
