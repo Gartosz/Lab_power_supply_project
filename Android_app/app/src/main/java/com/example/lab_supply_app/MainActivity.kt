fdbe6b6ef38e006159424474a9c68db888cb874d
Gartosz <bartekg001@gmail.com>
2023-05-02 01:20:15 +0200

    Use bottom navigation.

--- Android_app/app/src/main/java/com/example/lab_supply_app/MainActivity.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.lab_supply_app.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -20,5 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        bottomNav?.setupWithNavController(navController)
     }
 }
