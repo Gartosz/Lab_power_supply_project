101c99faa512e5b94db495e44678081b52bf5d37
Gartosz <bartekg001@gmail.com>
2023-05-02 01:19:57 +0200

    Handle fragment container.

--- Android_app/app/src/main/java/com/example/lab_supply_app/MainActivity.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.lab_supply_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.lab_supply_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        navHostFragment.findNavController()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
