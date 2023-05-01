7775e631760e9ed1493bc7e82c45e8607ecbe25b
Gartosz <bartekg001@gmail.com>
2023-05-02 01:16:26 +0200

    Add binding to main activity.

--- Android_app/app/src/main/java/com/example/lab_supply_app/MainActivity.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.lab_supply_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lab_supply_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
