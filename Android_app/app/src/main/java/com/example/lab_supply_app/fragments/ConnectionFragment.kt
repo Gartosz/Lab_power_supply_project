8f858657cac5a900dddc05cae40d7aa771789993
Bartosz <bartekg001@gmail.com>
2023-05-07 14:29:49 +0200

    Check and request Bluetooth permissions.

--- Android_app/app/src/main/java/com/example/lab_supply_app/fragments/ConnectionFragment.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/fragments/ConnectionFragment.kt
@@ -1,5 +1,6 @@
 package com.example.lab_supply_app.fragments
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.pm.PackageManager
@@ -8,6 +9,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -18,6 +21,8 @@ class ConnectionFragment : Fragment() {
     private lateinit var binding: ConnectionLayoutBinding
     private val connectionViewModel: ConnectionsViewModel by viewModels()
     private lateinit var bluetoothAdapter : BluetoothAdapter
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +34,20 @@ class ConnectionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    Toast.makeText(binding.root.context, "Wymagane jest przyznanie uprawnień " +
+                            "do połączenia i skanowania Bluetooth.", Toast.LENGTH_LONG).show()
+                }
+            }
+        requestPermission(Manifest.permission.BLUETOOTH_SCAN)
+        requestPermission(Manifest.permission.BLUETOOTH_CONNECT)
         }
 
     private fun requestPermission(permission: String)
@@ -62,5 +81,6 @@ class ConnectionFragment : Fragment() {
     private fun showBLEDevices()
     {
 
+    }
 
 }
