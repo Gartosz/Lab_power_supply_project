aa8a194cd54e6904c5277332f1b4fefbcee236cd
Gartosz <>
2023-05-19 23:24:48 +0200

    Implement class for basic handling BLE scan callback.

--- Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/BleScanCallback.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/BleScanCallback.kt
@@ -1,4 +1,26 @@
 package com.example.lab_supply_app.bluetooth
 
-class BleScanCallback {
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.util.Log
+
+class BleScanCallback(private val onBatchScanResultAction: (MutableList<ScanResult>?) -> Unit = {},
+                      private val onScanFailedAction: (Int) -> Unit = {},
+                      private val onScanResultAction: (ScanResult?) -> Unit = {}) : ScanCallback() {
+
+    override fun onScanResult(callbackType: Int, result: ScanResult?) {
+        super.onScanResult(callbackType, result)
+        onScanResultAction(result)
+    }
+
+    override fun onScanFailed(errorCode: Int) {
+        super.onScanFailed(errorCode)
+        Log.d("scan failed", errorCode.toString())
+        onScanFailedAction(errorCode)
+    }
+
+    override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+        super.onBatchScanResults(results)
+        onBatchScanResultAction(results)
+    }
 }
