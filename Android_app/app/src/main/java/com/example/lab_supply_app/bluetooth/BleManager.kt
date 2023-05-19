a7269c4a7ee13b4e1c5b380e9278d6fd42cead65
Gartosz <>
2023-05-19 23:40:18 +0200

    Implement BLE manager class to scan for devices.

--- Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/BleManager.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/BleManager.kt
@@ -1,4 +1,43 @@
 package com.example.lab_supply_app.bluetooth
 
-class BleManager {
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+
+class BleManager(private val bluetoothAdapter: BluetoothAdapter,
+                 private val scanButton: Button,
+                 private val textArray: Array<String>,
+                 private val BLEScanCallback: BleScanCallback = BleScanCallback()){
+
+    private lateinit var BleScanner : BluetoothLeScanner
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var isScanning: Boolean = false
+    @SuppressLint("MissingPermission")
+    fun scanBleDevices() {
+        BleScanner = bluetoothAdapter.bluetoothLeScanner
+
+        if (isScanning) {
+
+            isScanning = false
+            scanButton.text = textArray[0]
+            stopScan(BleScanner)
+
+        } else {
+            scanButton.text = textArray[1]
+            handler.postDelayed({ stopScan(BleScanner) }, 10000)
+
+            isScanning = true
+            BleScanner.startScan(BLEScanCallback)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun stopScan(BleScanner: BluetoothLeScanner) {
+        BleScanner.stopScan(BLEScanCallback)
+        handler.removeCallbacksAndMessages(null)
+    }
 }
