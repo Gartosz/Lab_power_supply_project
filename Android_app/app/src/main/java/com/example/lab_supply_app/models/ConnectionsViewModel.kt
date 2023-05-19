4bf8df0d508088da00d100218bab055f8bea884b
Gartosz <>
2023-05-19 23:40:47 +0200

    Add BLE devices live data to view model.

--- Android_app/app/src/main/java/com/example/lab_supply_app/models/ConnectionsViewModel.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/models/ConnectionsViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 
 class ConnectionsViewModel(application: Application) : AndroidViewModel(application) {
     val bluetoothState : MutableLiveData<Boolean> by lazy { MutableLiveData<Boolean>() }
+    val BLEDevices : MutableLiveData<MutableList<String>> = MutableLiveData(mutableListOf())
 
     fun setBTState(bluetoothAdapter: BluetoothAdapter)
     {
