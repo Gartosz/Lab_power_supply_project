17f5d156c4add562370289af4ab8190e285ec78c
Gartosz <bartekg001@gmail.com>
2023-06-12 03:31:50 +0200

    Set adapter and handle GATT callback.

--- Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/BleService.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/BleService.kt
@@ -1,8 +1,11 @@
 package com.example.lab_supply_app.bluetooth
 
+import android.annotation.SuppressLint
 import android.app.Service
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothProfile
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
@@ -31,6 +34,36 @@ class BleService(): Service() {
     inner class LocalBinder : Binder() {
         fun getService() : BleService {
             return this@BleService
+        }
+    }
+
+    fun setAdapter(adapter: BluetoothAdapter?): Boolean {
+        bluetoothAdapter = adapter
+        return adapter != null
+    }
+
+    private val bluetoothGattCallback = object : BluetoothGattCallback() {
+
+        @SuppressLint("MissingPermission")
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            super.onConnectionStateChange(gatt, status, newState)
+
+            bluetoothGatt = gatt
+
+            when (newState) {
+                BluetoothProfile.STATE_CONNECTING -> connectMessage.value = "CONNECTING"
+
+                BluetoothProfile.STATE_CONNECTED -> {
+                    connectMessage.value = "CONNECTED"
+                    bluetoothGatt?.discoverServices()
+                }
+
+                BluetoothProfile.STATE_DISCONNECTING -> connectMessage.value = "DISCONNECTING"
+
+                BluetoothProfile.STATE_DISCONNECTED -> connectMessage.value = "DISCONNECTED"
+
+                else -> connectMessage.value = "UNKNOWN"
+            }
         }
     }
 
