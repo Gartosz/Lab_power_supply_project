8adbf6e24ac1986247de4cc02f1a3815d46e12ee
Gartosz <bartekg001@gmail.com>
2023-06-12 03:26:49 +0200

    Add reset and getAddress method to easier change livedata pair.

--- Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/ConnectedDevice.kt
+++ Android_app/app/src/main/java/com/example/lab_supply_app/bluetooth/ConnectedDevice.kt
@@ -3,12 +3,21 @@ package com.example.lab_supply_app.bluetooth
 import androidx.lifecycle.MutableLiveData
 
 object ConnectedDevice {
-    val connectedDevice : MutableLiveData<Pair<String, String>> = MutableLiveData(Pair("", ""))
+    val connectedDevice = MutableLiveData(Pair("", ""))
 
     fun handleDeviceUpdate(name: String, address: String)
     {
         if (connectedDevice.value?.second == "" || address == connectedDevice.value?.second)
             connectedDevice.postValue(Pair(name, address))
+    }
 
+    fun reset()
+    {
+        connectedDevice.postValue(Pair("", ""))
+    }
+
+    fun getAddress(): String? {
+        val address = connectedDevice.value?.second
+        return if (address != "") address else null
     }
 }
